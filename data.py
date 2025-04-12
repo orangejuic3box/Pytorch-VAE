6b48c28b8a08c4cc3e2f2f6d46339e07313a6f54
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2025-04-11 19:31:48 -0600

    update for experiments

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ from torchvision import datasets, transforms # for classification datasets
 from noisy_clean_dataset import get_nc_datasets
 
 #load and process noisy_clean datasets
-nc_train, nc_test, nc_val = get_nc_datasets()
+nc_train, nc_test, nc_val, nc_exp = get_nc_datasets()
 
 _CIFAR_TRAIN_TRANSFORMS = [
     transforms.RandomCrop(32, padding=4),
@@ -43,8 +43,15 @@ VAL_DATASETS = {
     "noisy_clean": nc_val
 }
 
+EXPERIMENT_DATASETS = {
+    "noisy_clean": nc_exp #may need to change batch_sizer
+    #this is nc_val and nc_test combines but
+    #its fine bc i never used them
+}
+
 
 DATASET_CONFIGS = {
+    #size = 32x32 pixels, 3 channels
     'cifar10': {'size': 32, 'channels': 3, 'classes': 10},
     'noisy_clean': {'size': 32, 'channels': 3}  # No 'classes' needed
 }
