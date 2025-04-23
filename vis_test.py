5dbfe0996653c168ae9af1d28360ab621b13b2ff
Maggie Lacson <58317354+orangejuic3box@users.noreply.github.com>
2025-04-23 15:15:02 -0600

    Update vis_test.py

--- vis_test.py
+++ vis_test.py
@@ -10,10 +10,6 @@ def tensor_to_image(tensor):
     # Assuming tensor is a [C, H, W] and you need to convert it to numpy array
     tensor = tensor.cpu().detach()  # Detach from graph and move to CPU
     # tensor = tensor.numpy().transpose(1, 2, 0)  # Convert to HWC format for display
-
-    print("in tensor_to_image")
-    print(tensor.shape)
-
     return tensor
 
 # Connect to Visdom
@@ -23,39 +19,45 @@ vis = visdom.Visdom()
 if not vis.check_connection():
     print("Visdom server is not running. Please start it.")
 else:
-    train_loader, _, _ = get_nc_datasets()
+    train_loader, _, _, experiment_loader = get_nc_datasets()
 
     print("INSIDE VIS TEST")
     j=0
     imgs = []
+    set = 0
+
+    loader = experiment_loader #train_loader
+
+    print("loader is", len(loader), "long")
     
     # Get the first batch from the DataLoader
-    for i, (image, clean) in enumerate(train_loader):
-        print("---------------")
-        # Check the type of your image
-        print("image is type", type(image))  # It should output something like <class 'torch.Tensor'>
+    for i, (image, clean) in enumerate(loader):
+        if i%11 == 0 and  i!=0:
+            imgs = []
+        # print(f"--------{i} set {set}-------")
 
-        print("Image shape:", image.shape)
-        print("Image dtype:", image.dtype)
-        # Assuming your image is in a tensor format
-        print("Min value:", image.min().item())  # Get the minimum pixel value
-        print("Max value:", image.max().item())  # Get the maximum pixel value
-        if i%11 == 0:  # Access the first batch
-            j+=1
-            image = image[0]
-            clean = clean[0]
-
-            # Convert the first image in the batch (assuming batch size > 1)
-            img = tensor_to_image(image)  # Get the first image from the batch
-            cln = tensor_to_image(clean)
+        # print("Image shape:", image.shape)
+       
+        img = tensor_to_image(image[0])
+        imgs.append(img)
+        if (i+1) %11 == 0 and i!=0:  # Access the first batch
+            set = i//11
+            cln = tensor_to_image(clean[0])
             imgs.append(cln)
 
-    # Optional: pause to see the image in the browser
-    input("Press Enter to continue...")  # This ensures you can view the image before exiting.
-    img_tensors = torch.stack(imgs)
+            #############
+            print(type(image), type(clean))
+            #############
 
-    # Display the image using Visdom
-    visualize_images(img_tensors, f"{j} Output Images", w=800, h=800)
+            # Optional: pause to see the image in the browser
+            usr = input("Press Enter to continue...")  # This ensures you can view the image before exiting.
+            if usr =="q":
+                exit()
+    
+            img_tensors = torch.stack(imgs)
+
+            # Display the image using Visdom
+            visualize_images(img_tensors, f"Set{set} Noisy-Clean", w=400, h=400)
 
     # vis.image(img, opts=dict(title="Input Image"))
     # vis.image(cln, opts=dict(title=f"Output Image {j}"))
